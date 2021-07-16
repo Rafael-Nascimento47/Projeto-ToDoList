ac7afdb6643b66e37250d493ac991a44ae96d557
Rafael-Nascimento47 <rafael.nascimento5458@gmail.com>
2021-07-15 22:18:30 -0300

    Adicinado função de criar tarefa na lista

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,11 +1,19 @@
 const criarTarefa = (evento) => {
     
     evento.preventDefault()
+
+    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valorInput = input.value
-    console.log(valorInput)
-    input.value = " "
+    
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    
+    const conteudo = `<p class="content">${valorInput}</p>`
 
+    tarefa.innerHTML = conteudo
+    lista.appendChild(tarefa)
+    input.value = " "
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
