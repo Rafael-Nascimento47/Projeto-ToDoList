99fea6a81c22de4698621987e63df1137b869f36
Rafael-Nascimento47 <rafael.nascimento5458@gmail.com>
2021-07-21 18:32:10 -0300

    Adicionado animação ao pressionar 'concluir'

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -27,6 +27,14 @@ const BotaoConclui = () => {
     botaoConclui.classList.add('check-button')
     botaoConclui.innerText = 'Concluir'
 
-    botaoConclui.addEventListener('click', () => {console.log("fui clicado")})
+    botaoConclui.addEventListener('click', concluirTarefa)
     return botaoConclui
 }
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+    const tarefaConcluida = botaoConclui.parentElement
+
+    tarefaConcluida.classList.toggle('done')
+
+}   
