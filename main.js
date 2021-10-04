cac64ac1d562902ce740e73176f13c037c1da7b3
Rafael-Nascimento47 <rafael.nascimento5458@gmail.com>
2021-10-04 14:00:06 -0300

    Mudança de SessionStorage para LocalStorage

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const handleNovoItem = (evento) => {
 
     lista.appendChild(criaTarefa)
 
-    sessionStorage.setItem("tarefas",JSON.stringify(dados))
+    localStorage.setItem("tarefas",JSON.stringify(dados))
 
     input.value = " "
 
